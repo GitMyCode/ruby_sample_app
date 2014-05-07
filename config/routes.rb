c2c86e611600fe2d89b66c59951a2a360389658d
gitmycode <marbouchl@gmail.com>
2014-05-07 18:15:56 -0400

    je viens d'ajouter le remember_token a l'utilisateur avec sa creation et le test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Untitled2::Application.routes.draw do
 
   resources :users
+  resources :sessions , only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
-
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   get "users/new"
   root  to:'static_pages#home'
