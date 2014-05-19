de6f8fbe189353d372d9dbb7315a42cd67005cd4
GitMyCode <marbouchl@gmail.com>
2014-05-18 21:12:27 -0400

    gemlock et populate script

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Untitled2::Application.routes.draw do
 
   resources :users
   resources :sessions , only: [:new, :create, :destroy]
-  get 'users', to:'users#index'
+  get 'users', to:'users#index' #probablement inutil a cause que resources le fait deja
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
